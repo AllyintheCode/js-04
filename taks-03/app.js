7395a44e3f5bd7ac7a22fdd68e3be90a7bd4b9d2
AllyintheCode <allyasadzade@gmail.com>
2025-03-05 02:33:15 +0400

    taks-03 complated

---
+++ taks-03/app.js
@@ -0,0 +1,6 @@
+function ToRublDollarChanges(rubl, dollar) {
+  console.log(`Sizin umumi rubl depozitiniz : ${rubl + rubl * 89.75} Rubldur`);
+  console.log(`Sizin umumi dollar depozitiniz : ${dollar + dollar * 0.011} Dollardir`);
+}
+ToRublDollarChanges(10, 100)
+ToRublDollarChanges(20, 40)
