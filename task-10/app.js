fcc9c59edb4df486a8b063de8f08f7f1764bb322
AllyintheCode <allyasadzade@gmail.com>
2025-03-05 04:09:20 +0400

    task 10 complated

---
+++ task-10/app.js
@@ -0,0 +1,14 @@
+function reqem(eded) {
+  if (eded > 0) {
+    console.log("sizin edediniz musbetdir");
+  }
+  else if (eded < 0) {
+    console.log("sizin edediniz menfidir");
+  }
+  else if (eded == 0) {
+    console.log("sizin edediniz sifira beraberdir");
+  }
+}
+reqem(5)
+reqem(0)
+reqem(-5)
